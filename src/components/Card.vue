4b3b57989baaf06c3f7a7d2125332059b32e5a18
Sulamita Althaus <40037816+SulamitaAlthaus@users.noreply.github.com>
2021-02-11 16:39:23 -0300

    conceitos de vuejs

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-col>
+      <b-card 
+        :title="nome"
+        img-src="https://picsum.photos/600/300"
+        img-alt="Curso de VueJS"
+        style="max-width: 20rem"
+        img-top
+        tag="article"
+        class="mb-2"
+        border-variant="primary"
+        header="Curso"
+        header-bg-variant="primary"
+        header-text-variant="white"
+        align="center"
+      >
+        <b-card-text>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere nemo
+          alias amet ipsa reiciendis quas cupiditate voluptatem impedit labore
+          saepe fuga laboriosam, temporibus, molestias tempora dolore. Repellat
+          quasi vero temporibus!
+        </b-card-text>
+        <b-button variant="primary">Comprar</b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nome: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <card v-for="curso in cursos" :key="curso.id" :nome="curso.name"></card>
+    
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue';
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      cursos: []
+    }
+  },
   components: {
-    HelloWorld
+    Card
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods: {
+    async fetchData(){
+      const res = await fetch("cursos.json");
+      const val = await res.json();
+      this.cursos = val;
+    }
   }
-}
+};
 </script>
